07cc1b71d301f124009059971118b52ff1241d03
Ankit Poudel <crankit694@gmail.com>
2026-03-02 19:37:55 +0545

    Added Home Page

---
+++ src/Pages/Auth.jsx
@@ -0,0 +1,7 @@
+function Auth() {
+  return (
+    <div>Auth Page</div>
+  )
+}
+
+export default Auth
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+    return (
+        <nav className='navbar'>
+            <div className='navbar-container'>
+                <Link to="/" className="navbar-brand">SwanHub</Link>
+            <div className='navbar-links'>
+                <Link to="/" className='navbar-link' >Home</Link>
+                <Link to="/checkout" className='navbar-link'>Cart</Link>
+            </div>
+            <div className='navbar-auth'>
+                <div className='navbar-auth-links'>
+                    <Link to="/auth" className='btn btn-secondary'>Login</Link>
+                    <Link to="/auth" className='btn btn-primary'>Signup</Link>
+                </div>
+
+            </div>
+         </div>
+        </nav>
+    );
+}
+
+export default Navbar
